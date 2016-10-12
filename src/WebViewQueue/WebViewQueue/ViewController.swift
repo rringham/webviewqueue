74a128a408580501976adf834d9daa4e11cb2709
Rob Ringham <rringham@infusion.com>
2016-10-12 10:05:07 -0400

    update

--- src/WebViewQueue/WebViewQueue/ViewController.swift
+++ src/WebViewQueue/WebViewQueue/ViewController.swift
@@ -8,9 +8,11 @@
 import Foundation
 import UIKit
 import WebKit
+import JavaScriptCore
 
 class ViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate {
 
+    private var context: JSContext! = JSContext()
     private var webView: WKWebView!
     private var contentController: WKUserContentController!
     var operationsStarted = 0
@@ -23,12 +25,16 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate {
         contentController = WKUserContentController()
         contentController.addScriptMessageHandler(self, name: "continueHandler")
         config.userContentController = contentController
-        webView = WKWebView(
-            frame: self.view.frame,
-            configuration: config
-        )
+        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1, height: 1), configuration: config)
         webView.UIDelegate = self
         webView.loadHTMLString(getHtml(), baseURL: nil)
+        view.addSubview(webView)
+        
+        context.setObject(Test.self, forKeyedSubscript: "Test")
+        context.exceptionHandler = { context, exception in
+            print("JS Error: \(exception)")
+        }
+        context.evaluateScript(getJS())
     }
     
     func webView(webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: () -> Void) {
@@ -75,13 +81,33 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate {
         // approach 2: serial request queuing
         // result: all complete successfully, no WKWebView hang
         //
-        for i in 1...100 {
+        
+        for i in 1...10 {
             print("starting main js operation \(i)")
             self.operationsStarted += 1
-            self.webView.evaluateJavaScript("someLongRunningProcess(3000, \(i));", completionHandler: { (result, error) in
+            self.webView.evaluateJavaScript("someLongRunningProcess(100, \(i));", completionHandler: { (result, error) in
                 print("evaluateJavaScript completion handler for main js operation \(i) done")
             })
         }
+        
+        for _ in 1...100 {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(1) * NSEC_PER_MSEC)), dispatch_get_main_queue(), { () -> Void in
+                print("pinging JS")
+                self.webView.evaluateJavaScript("ping();", completionHandler: nil)
+            })
+        }
+    }
+    
+    @IBAction func hammerJavaScriptCore(sender: AnyObject) {
+        dispatch_async(dispatch_get_main_queue(), {
+            for i in 1...10 {
+                dispatch_async(dispatch_get_main_queue(), {
+                    print("starting main js operation \(i)")
+                    self.operationsStarted += 1
+                    self.context.evaluateScript("someLongRunningProcess(\(i));")
+                })
+            }
+        })
     }
     
     @IBAction func stats(sender: AnyObject) {
@@ -97,6 +123,35 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate {
         catch {
             return ""
         }
+    }
+    
+    func getJS() -> String {
+        let fileLocation = NSBundle.mainBundle().pathForResource("poc", ofType: "js")!
+        do {
+            return try String(contentsOfFile: fileLocation)
+        }
+        catch {
+            return ""
+        }
+    }
+}
+
+// JSCore setTimeout http://stackoverflow.com/questions/15991044/ios-implemention-of-window-settimeout-with-javascriptcore
+@objc protocol TestJSExports : JSExport {
+    static func setTimeout(_ cb: JSValue, _ wait: Int) -> ()
+    static func test(result: String) -> ()
+}
+
+@objc class Test : NSObject, TestJSExports {
+    class func setTimeout(_ cb: JSValue, _ wait: Int) -> () {
+        let callback = cb as JSValue
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(wait) * NSEC_PER_MSEC)), dispatch_get_main_queue(), { () -> Void in
+            callback.callWithArguments([])
+        })
+    }
+    
+    class func test(result: String) -> () {
+        print("pid \(result) callback")
     }
 }
 
